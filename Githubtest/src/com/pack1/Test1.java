6850fa0f3dd0e82718f88ac4094adf997111776c
my pc <my pc@DESKTOP-V1AG363>
2019-12-20 18:06:19 +0530

    C class added

--- Githubtest/src/com/pack1/Test1.java
+++ Githubtest/src/com/pack1/Test1.java
@@ -1,11 +1,33 @@
 package com.pack1;
 
+  class D{
+	 
+	  }
+  
+    /*class G extends D{
+    	int a=30;
+    	G(){
+   
+    	System.out.println(a);
+    	}
+	
+}*/
+
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("hell world");
-		System.out.println("hell world2");
-		System.out.println("hell world22");
+		
+	//	D g1= new D();
+		// g1.add("ravi", "30");
+		for(int i=1;i<=1;i++){  
+			//loop of j  
+			for(int j=1;j<=3;j++){  
+			       // System.out.println(i+" "+j);  
+			        System.out.println("c");  
 	}
-
+			for(int j=1;j<=7;j++){  
+			       // System.out.println(i+" "+j);  
+			        System.out.println("b");  
+	}
+}}
 }
